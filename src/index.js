83ff2cb781d6b02b38ae7157f8b9975dc7732de9
Odinakachukwu Ezeobika <solomonzbk@gmail.com>
2020-07-09 14:51:45 +0100

    Setup Redux

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import * as serviceWorker from "./serviceWorker";
+import BaseRoute from "./routes/BaseRoute";
+import store from "./store";
+
+// Styles
+// import "bootstrap/dist/css/bootstrap.css";
+// import "./assets/stylesheets/Common.scss";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <BaseRoute />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
