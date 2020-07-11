5e08fbe78a34cbf9705f51f3283c836d9b034693
Odinakachukwu Ezeobika <solomonzbk@gmail.com>
2020-07-11 18:40:39 +0100

    Style header component

--- src/views/components/elements/Header.jsx
+++ src/views/components/elements/Header.jsx
@@ -2,8 +2,17 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import SearchInput from "./SearchInput";
 import StarwarsLogo from "./StarwarsLogo";
+import { SlideInDown } from "../../../helpers/animations";
 
 class Header extends Component {
+  componentDidMount() {
+    const urlPath = this.getUrlpath();
+    if (urlPath) {
+      const header = document.querySelector(".navbar");
+      header.style.backgroundColor = "#000000";
+    }
+  }
+
   getUrlpath = () => {
     const urlDetails = window.location.pathname.split("/");
     return urlDetails[urlDetails.length - 1];
@@ -13,45 +22,49 @@ class Header extends Component {
     const urlPath = this.getUrlpath();
     return (
       <header>
-        <nav className="navbar navbar-expand-lg navbar-dark fixed-top">
+        <nav className="navbar navbar-expand-lg navbar-dark shadow-lg fixed-top">
           <div className="container">
-            <Link to="/">
-              <StarwarsLogo className="navbar-brand" width="100" />
-            </Link>
+            <SlideInDown>
+              <Link to="/">
+                <StarwarsLogo className="navbar-brand" width="100" />
+              </Link>
+            </SlideInDown>
 
-            <button
-              className="navbar-toggler"
-              type="button"
-              data-toggle="collapse"
-              data-target="#navbarNav"
-              aria-controls="navbarNav"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarNav">
-              <ul className="navbar-nav ml-auto">
-                <li className="nav-item mr-2">
-                  {urlPath ? <SearchInput /> : null}
-                </li>
-                <li className="nav-item">
-                  <Link to="/characters" className="nav-link text-white">
-                    Characters
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link to="/planets" className="nav-link text-white">
-                    Planets
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link to="/starships" className="nav-link text-white">
-                    Starships
-                  </Link>
-                </li>
-              </ul>
-            </div>
+            <SlideInDown>
+              <button
+                className="navbar-toggler"
+                type="button"
+                data-toggle="collapse"
+                data-target="#navbarNav"
+                aria-controls="navbarNav"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+              >
+                <span className="navbar-toggler-icon"></span>
+              </button>
+              <div className="collapse navbar-collapse" id="navbarNav">
+                <ul className="navbar-nav ml-auto">
+                  <li className="nav-item mr-2">
+                    {urlPath ? <SearchInput /> : null}
+                  </li>
+                  <li className="nav-item">
+                    <Link to="/characters" className="nav-link text-white">
+                      Characters
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link to="/planets" className="nav-link text-white">
+                      Planets
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link to="/starships" className="nav-link text-white">
+                      Starships
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            </SlideInDown>
           </div>
         </nav>
       </header>
